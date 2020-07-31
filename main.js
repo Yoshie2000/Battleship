0cec92bfc8aa81ef5490e07dccd020b281081ff1
Patrick Leonhardt <patrick.leonhardt@gmx.net>
2020-07-31 20:37:52 +0200

    Initial setup & Hello World

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    fullscreen();
+}
+
+function draw() {
+    background(200);
+
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text("Hello World", width / 2, height / 2);
+}
