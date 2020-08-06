7aa8659a95ea35b8547e53ba49c65536ea48ef33
Patrick Leonhardt <patrick.leonhardt@gmx.net>
2020-08-06 17:14:22 +0200

    Cleanup & Comments

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 /// <reference path="p5.js" />
 
+// How many grid cells per axis
 let gridSize = 10;
 
+// All the ships on the field
 let ship = new Ship();
 
+// Temporary data for dragging ships with the mouse
 let dragStatus = {
     ship: undefined,
     dragging: false
 }
 
 function setup() {
+    // Create a fullscreen canvas
     createCanvas(windowWidth, windowHeight);
     fullscreen();
 }
 
 function draw() {
+    // Blue background
     background(0, 180, 255);
 
+    // Draw the field
     drawField();
 
+    // Draw the ships
     ship.draw();
 }
 
@@ -27,26 +34,32 @@ function draw() {
 function drawField() {
     let gridCellSize = getGridCellSize();
 
+    // The offset from the top left of the browser window
     let xOffset = (width - (gridCellSize * gridSize)) / 2;
     let yOffset = (height - (gridCellSize * gridSize)) / 2;
 
     for (let x = 0; x < gridSize; x++) {
         for (let y = 0; y < gridSize; y++) {
 
+            // White, thin stroke
             stroke(255);
             fill(255);
             strokeWeight(1);
 
+            // Text configuration
             textAlign(CENTER, CENTER);
             textSize(gridCellSize * 0.75);
 
+            // Draw the horizontal text (1 2 3 4 .. gridSize)
             if (y == 0) {
                 text(x + 1, x * gridCellSize + xOffset + gridCellSize / 2, y * gridCellSize + yOffset - gridCellSize / 2);
             }
+            // Draw the vertical text (A B C D .. alphabet(gridSize))
             if (x == 0) {
                 text(getLetter(y + 1), x * gridCellSize + xOffset - gridCellSize / 2, y * gridCellSize + yOffset + gridCellSize / 2);
             }
 
+            // White, thicker stroke, no fill for the grid
             stroke(255);
             strokeWeight(5);
             noFill();
@@ -59,14 +72,16 @@ function drawField() {
 
 //#endregion Field
 
-//#region Mouse Interactions
+//#region Mouse
 
 function mouseClicked() {
+    // When in drag mode, try to exit
     if (dragStatus.dragging) {
         stopDrag();
         return;
     }
     
+    // If the cursor is above a ship, start dragging it
     let shipCanvasBounds = ship.getCanvasBounds();
     if (rectContains(shipCanvasBounds, mouseX, mouseY)) {
         startDrag(ship, shipCanvasBounds);
@@ -75,9 +90,10 @@ function mouseClicked() {
 
 //#endregion Mouse Interactions
 
-//#region Keyboard Interactions
+//#region Keyboard
 
 function keyPressed() {
+    // Rotate the current dragged ship when "R" is pressed
     if (dragStatus.dragging && (key == "r" || key == "R")) {
         dragStatus.ship.rotate();
     }
@@ -88,6 +104,7 @@ function keyPressed() {
 //#region Ship Drag
 
 function startDrag(ship, shipCanvasBounds) {
+    // The offset from the top left of the ship
     let mouseOffsetX = shipCanvasBounds.startX - mouseX;
     let mouseOffsetY = shipCanvasBounds.startY - mouseY;
 
@@ -97,7 +114,10 @@ function startDrag(ship, shipCanvasBounds) {
 }
 
 function stopDrag() {
+    // The bounds of the ship if dragging stopped now
     let newShipFieldCoords = canvasCoordsToFieldCoords(mouseX + dragStatus.ship.dragOffsetX, mouseY + dragStatus.ship.dragOffsetY);
+    
+    // Check if the ship would land in the field
     if (!isInFieldBounds(newShipFieldCoords.x, newShipFieldCoords.y)
         || !isInFieldBounds(newShipFieldCoords.x + dragStatus.ship.width - 1, newShipFieldCoords.y)
         || !isInFieldBounds(newShipFieldCoords.x, newShipFieldCoords.y + dragStatus.ship.height - 1)
@@ -111,8 +131,9 @@ function stopDrag() {
 
 //#endregion Ship Drag
 
-//#region Geometry Calculations
+//#region Geometry
 
+// Checks if the coordinates are inside of the field
 function isInFieldBounds(x, y) {
     return rectContains({
         startX: 0, 
@@ -128,8 +149,9 @@ function rectContains(bounds, x, y) {
 
 //#endregion Geometry Calculations
 
-//#region Canvas Calculations
+//#region Canvas
 
+// Translates field coordinates to canvas coordinates
 function fieldCoordsToCanvasCoords(x, y) {
     let gridCellSize = getGridCellSize();
 
@@ -142,6 +164,7 @@ function fieldCoordsToCanvasCoords(x, y) {
     };
 }
 
+// Translates canvas coordinates to field coordinates
 function canvasCoordsToFieldCoords(x, y, floorResult) {
     let gridCellSize = getGridCellSize();
 
@@ -154,10 +177,12 @@ function canvasCoordsToFieldCoords(x, y, floorResult) {
     };
 }
 
+// Calculates the size of one grid cell
 function getGridCellSize() {
     return (min(width, height) - 250) / gridSize;
 }
 
+// Returns the n-th letter of the alphabet (starting at 0)
 function getLetter(index) {
     return String.fromCharCode(64 + index);
 }
--- ship.js
+++ ship.js
@@ -1,5 +1,6 @@
 class Ship {
 
+    // Set up the variables
     constructor() {
         this.width = 1;
         this.height = 3;
@@ -17,7 +18,10 @@ class Ship {
         this.dragOffsetY = 0;
     }
 
+    //#region Drawing
+
     draw() {
+        // Make the ship follow the mouse while dragging
         if (this.dragging) {
             this.canvasX = mouseX;
             this.canvasY = mouseY;
@@ -27,6 +31,7 @@ class Ship {
 
         for (let shipTileX = this.fieldX; shipTileX < this.fieldX + this.width; shipTileX++) {
             for (let shipTileY = this.fieldY; shipTileY < this.fieldY + this.height; shipTileY++) {
+                // When dragging, follow the mouse and take the offset into account; otherwise stay at the current tile
                 let canvasCoords = this.dragging ? 
                     {
                         x: this.canvasX + this.dragOffsetX + (shipTileX - this.fieldX) * gridCellSize,
@@ -34,6 +39,7 @@ class Ship {
                     }
                     : fieldCoordsToCanvasCoords(shipTileX, shipTileY);
 
+                // Black boxes for each ship tile
                 stroke(0);
                 strokeWeight(1);
                 fill(0);
@@ -44,19 +50,9 @@ class Ship {
         }
     }
 
-    rotate() {
-        let oldWidth = this.width;
-        let oldHeight = this.height;
+    //#endregion Drawing
 
-        this.width = oldHeight;
-        this.height = oldWidth;
-
-        let dragOffsetRatioX = this.dragOffsetX / oldWidth;
-        let dragOffsetRatioY = this.dragOffsetY / oldHeight;
-
-        this.dragOffsetX = this.width * dragOffsetRatioX;
-        this.dragOffsetY = this.height * dragOffsetRatioY;
-    }
+    //#region Dragging
 
     startDrag(dragOffsetX, dragOffsetY) {
         this.dragging = true;
@@ -70,7 +66,30 @@ class Ship {
         this.fieldY = newFieldCoords.y;
     }
 
+    //#endregion Dragging
+
+    //#region Geometry
+
+    rotate() {
+        // Store the old width and height
+        let oldWidth = this.width;
+        let oldHeight = this.height;
+
+        // Switch width and height around
+        this.width = oldHeight;
+        this.height = oldWidth;
+
+        // Calculate the ratio between the drag offset and width / height
+        let dragOffsetRatioX = this.dragOffsetX / oldWidth;
+        let dragOffsetRatioY = this.dragOffsetY / oldHeight;
+
+        // Apply that ratio to the updated width and height
+        this.dragOffsetX = this.width * dragOffsetRatioX;
+        this.dragOffsetY = this.height * dragOffsetRatioY;
+    }
+
     getFieldBounds() {
+        // The rectangle of the ship inside the field
         return {
             startX: this.fieldX,
             startY: this.fieldY,
@@ -83,6 +102,7 @@ class Ship {
         let canvasCoords = fieldCoordsToCanvasCoords(this.fieldX, this.fieldY);
         let gridCellSize = getGridCellSize();
 
+        // The rectangle of the ship inside the canvas
         return {
             startX: canvasCoords.x,
             startY: canvasCoords.y,
@@ -90,5 +110,7 @@ class Ship {
             endY: canvasCoords.y + gridCellSize * this.height
         }
     }
+
+    //#endregion Geometry
 
 }
