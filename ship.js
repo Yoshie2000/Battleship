e4aaab2cb148f59f5c58fca21f112bcba3838709
Patrick Leonhardt <patrick.leonhardt@gmx.net>
2020-08-06 18:56:56 +0200

    Added animations for dragging and rotating

--- ship.js
+++ ship.js
@@ -1,3 +1,5 @@
+/// <reference path="p5.js" />
+
 class Ship {
 
     // Set up the variables
@@ -16,6 +18,14 @@ class Ship {
 
         this.dragOffsetX = 0;
         this.dragOffsetY = 0;
+
+        this.stopDragAnimation = false;
+        this.stopDragAnimationTime = 0.1;
+        this.stopDragAnimationTimer = 0;
+
+        this.rotateAnimation = false;
+        this.rotateAnimationTime = 0.2;
+        this.rotateAnimationTimer = 0;
     }
 
     //#region Drawing
@@ -27,17 +37,59 @@ class Ship {
             this.canvasY = mouseY;
         }
 
+        let animationOffset = {
+            x: 0,
+            y: 0
+        };
+
+        push();
+
+        if (this.rotateAnimation) {
+            if (this.rotateAnimationTimer >= this.rotateAnimationTime) {
+                this.rotateAnimation = false;
+                this.rotateAnimationTimer = 0;
+            } else {
+                this.rotateAnimationTimer += deltaTime / 1000;
+                let timeRatio = this.easeInOut(this.rotateAnimationTimer / this.rotateAnimationTime);
+
+                translate(mouseX, mouseY);
+
+                let rotation = -HALF_PI + (timeRatio * HALF_PI);
+                rotate(rotation);
+
+                animationOffset.x -= mouseX;
+                animationOffset.y -= mouseY;
+            }
+        }
+
+        // Offset the ship for a stop drag animation
+        if (this.stopDragAnimation) {
+            if (this.stopDragAnimationTimer >= this.stopDragAnimationTime) {
+                this.stopDragAnimation = false;
+                this.stopDragAnimationTimer = 0;
+            } else {
+                this.stopDragAnimationTimer += deltaTime / 1000;
+                let timeRatio = this.easeInOut(this.stopDragAnimationTimer / this.stopDragAnimationTime);
+
+                animationOffset.x += (this.stopDragAnimationDestination.x - this.stopDragAnimationOrigin.x) * timeRatio;
+                animationOffset.y += (this.stopDragAnimationDestination.y - this.stopDragAnimationOrigin.y) * timeRatio;
+            }
+        }
+
         let gridCellSize = getGridCellSize();
 
         for (let shipTileX = this.fieldX; shipTileX < this.fieldX + this.width; shipTileX++) {
             for (let shipTileY = this.fieldY; shipTileY < this.fieldY + this.height; shipTileY++) {
+
                 // When dragging, follow the mouse and take the offset into account; otherwise stay at the current tile
-                let canvasCoords = this.dragging ? 
+                let canvasCoords = this.dragging || this.stopDragAnimation ? 
                     {
                         x: this.canvasX + this.dragOffsetX + (shipTileX - this.fieldX) * gridCellSize,
                         y: this.canvasY + this.dragOffsetY + (shipTileY - this.fieldY) * gridCellSize
                     }
                     : fieldCoordsToCanvasCoords(shipTileX, shipTileY);
+                canvasCoords.x += animationOffset.x;
+                canvasCoords.y += animationOffset.y;
 
                 // Black boxes for each ship tile
                 stroke(0);
@@ -57,6 +109,9 @@ class Ship {
 
             }
         }
+
+        pop();
+
     }
 
     //#endregion Drawing
@@ -71,8 +126,14 @@ class Ship {
 
     stopDrag(newFieldCoords) {
         this.dragging = false;
+        this.stopDragAnimation = true;
         this.fieldX = newFieldCoords.x;
         this.fieldY = newFieldCoords.y;
+        this.stopDragAnimationOrigin = {
+            x: this.canvasX + this.dragOffsetX,
+            y: this.canvasY + this.dragOffsetY
+        }
+        this.stopDragAnimationDestination = fieldCoordsToCanvasCoords(newFieldCoords.x, newFieldCoords.y);
     }
 
     //#endregion Dragging
@@ -95,6 +156,9 @@ class Ship {
         // Apply that ratio to the updated width and height
         this.dragOffsetX = this.width * dragOffsetRatioX;
         this.dragOffsetY = this.height * dragOffsetRatioY;
+
+        // Start animation
+        this.rotateAnimation = true;
     }
 
     getFieldBounds() {
@@ -121,5 +185,21 @@ class Ship {
     }
 
     //#endregion Geometry
+
+    //#region Timing
+
+    easeIn(k) {
+        return Math.pow(k, 1.675);
+    }
+
+    easeOut(k) {
+        return 1 - Math.pow(1 - k, 1.675);
+    }
+
+    easeInOut(k) {
+        return 0.5 * (sin(PI * Math.sin(k - 0.5)) + 1);
+    }
+
+    //#endregion Timing
 
 }
